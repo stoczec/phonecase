c8d98bae2734d997e4f171d4cd679701d7524453
Dmytro Herashchenko <stoczec@gmail.com>
2024-08-10 11:46:01 +0200

    add models User, Order, ShippingAddress, BillingAddress

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -1,6 +1,8 @@
 'use server'
 
+import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products'
 import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const createCheckoutSession = async ({
 	configId,
@@ -14,4 +16,18 @@ export const createCheckoutSession = async ({
 	if (!configuration) {
 		throw new Error('No such configuration found')
 	}
+
+	const { getUser } = getKindeServerSession()
+	const user = await getUser()
+
+	if (!user) {
+		throw new Error('You need to be logged in')
+	}
+
+	const { finish, material } = configuration
+
+	let price = BASE_PRICE
+	if (finish === 'textured') price += PRODUCT_PRICES.finish.textured
+	if (material === 'polycarbonate')
+		price += PRODUCT_PRICES.material.polycarbonate
 }
