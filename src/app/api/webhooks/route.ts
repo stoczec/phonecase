9b6f8b4fe415422db9b81a58e614041c73f044fd
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-01 23:08:26 +0200

    Update webhook route to change sender email, update favicon, and add metadata utility function in utils.ts

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
 			})
 
 			await resend.emails.send({
-				from: "LizardCase <stoczec@gmail.com>",
+				from: "CaseLizard <stoczec@gmail.com>",
 				to: [event.data.object.customer_details.email],
 				subject: "Thanks for your order!",
 				react: OrderReceivedEmail({
