05af24aaa7a30420a833e7fc8eccc57519c59281
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-04 22:09:57 +0200

    Refactor Navbar component: reorganized imports, updated branding, and simplified button styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,19 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { buttonVariants } from './ui/button'
-import { ArrowRight } from 'lucide-react'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const Navbar = async () => {
 	const { getUser } = getKindeServerSession()
 	const user = await getUser()
-
 	const isAdmin = user?.email === process.env.ADMIN_EMAIL
-
 	return (
 		<nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
 			<MaxWidthWrapper>
-				<div className="flex h-14 items-center justify-between border-b border-zinc-200">
+				<div className="flex h-14 items-center justify-between border-b border-zinc-200 ">
 					<Link href="/" className="flex z-40 font-semibold">
-						case<span className="text-green-600">cobra</span>
+						case<span className="text-blue-600">lizard</span>
 					</Link>
 
 					<div className="h-full flex items-center space-x-4">
@@ -23,20 +21,14 @@ const Navbar = async () => {
 							<>
 								<Link
 									href="/api/auth/logout"
-									className={buttonVariants({
-										size: 'sm',
-										variant: 'ghost',
-									})}
+									className={buttonVariants({ size: 'sm', variant: 'ghost' })}
 								>
 									Sign out
 								</Link>
 								{isAdmin ? (
 									<Link
 										href="/dashboard"
-										className={buttonVariants({
-											size: 'sm',
-											variant: 'ghost',
-										})}
+										className={buttonVariants({ size: 'sm', variant: 'ghost' })}
 									>
 										Dashboard ✨
 									</Link>
@@ -56,26 +48,18 @@ const Navbar = async () => {
 							<>
 								<Link
 									href="/api/auth/register"
-									className={buttonVariants({
-										size: 'sm',
-										variant: 'ghost',
-									})}
+									className={buttonVariants({ size: 'sm', variant: 'ghost' })}
 								>
 									Sign up
 								</Link>
 
 								<Link
 									href="/api/auth/login"
-									className={buttonVariants({
-										size: 'sm',
-										variant: 'ghost',
-									})}
+									className={buttonVariants({ size: 'sm', variant: 'ghost' })}
 								>
 									Login
 								</Link>
-
 								<div className="h-8 w-px bg-zinc-200 hidden sm:block" />
-
 								<Link
 									href="/configure/upload"
 									className={buttonVariants({
